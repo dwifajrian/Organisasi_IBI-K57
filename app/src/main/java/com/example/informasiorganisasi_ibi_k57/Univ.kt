2ac3ec97cfe17bb05328bf66085e95f4862e3f96
dwifajrian321@gmail.com <dwifajrian321@gmail.com>
2021-07-24 19:46:51 +0700

    first commit

---
+++ app/src/main/java/com/example/informasiorganisasi_ibi_k57/Univ.kt
@@ -0,0 +1,9 @@
+package com.example.informasiorganisasi_ibi_k57
+
+class Univ (
+    var name: String = "",
+    var rank: String = "",
+    var photo: String = "",
+    var overview: String = "",
+    var identity: String = ""
+)
